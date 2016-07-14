f4084bd1a3a04380b46faa6d3eb09f8c1a047e7d
Chris Wolfe <chriswwolfe@gmail.com>
2016-07-14 08:43:14 -0500

    remove all feedparsing, switch to using the json endpoints

--- gaspocket/bot.py
+++ gaspocket/bot.py
@@ -1,15 +1,10 @@
 from __future__ import absolute_import, division, print_function
 
 import os
-import re
 
-from datetime import datetime, timedelta
-
-from time import mktime
+from datetime import datetime
 
 import attr
-
-import feedparser
 
 import treq
 
@@ -21,164 +16,115 @@ from twisted.logger import Logger
 from twython import Twython
 
 # set the observers up in main
-log = Logger(namespace="gaspocket.bot")
+log = Logger(namespace=u'gaspocket.bot')
 
+GOOD = u'GOOD'
+BAD = u'BAD'
 
-REGEX = re.compile(r'\b(fixed|good|resolved)\b', re.IGNORECASE)
+STATUS_IO_GOOD = u'All Systems Operational'
+GITHUB_GOOD = u'good'
+
+GITHUB = u'https://status.github.com/api/status.json'
+CODECOV = u'https://wdzsn5dlywj9.statuspage.io/api/v2/status.json'
+TRAVIS = u'https://pnpcptp8xh9k.statuspage.io/api/v2/status.json'
 
 
 @attr.s
 class Context(object):
-    # state is a boolean, if true then stormy
-    alert_state = attr.ib()
+    state = attr.ib()
     last_update = attr.ib()
 
 
 @inlineCallbacks
 def http_json(url):
-    log.info("Calling {url}", url=url)
+    log.info(u'Calling {url}', url=url)
     response = yield treq.get(url)
-    log.info("Got json response for {url}", url=url)
+    log.info(u'Got json response for {url}', url=url)
     json_response = yield response.json()
     returnValue(json_response)
 
 
-@inlineCallbacks
-def http_content(url):
-    log.info("Calling {url}", url=url)
-    response = yield treq.get(url)
-    log.info("Got content response for {url}", url=url)
-    content = yield treq.content(response)
-    returnValue(content)
+def parse_statusio(status):
+    return status[u'status'][u'description']
 
 
-def parse_atom_feed(feed, threshold_time):
-    log.debug("{status}", status="parsing")
-    data = feedparser.parse(feed)
-    entries = [e for e in data["items"]]
-
-    filtered = []
-    for entry in entries:
-        entry_time_struct = entry['updated_parsed']
-        entry_time = datetime.fromtimestamp(mktime(entry_time_struct))
-
-        if entry_time > threshold_time:
-            filtered.append(entry)
-
-    log.debug("{status}", status="done parsing")
-    return filtered
+def parse_github(status):
+    return status[u'status']
 
 
 @inlineCallbacks
-def get_github_status():
-    parsed_json = yield http_json(b'https://status.github.com/api/status.json')
-    returnValue(parsed_json[u'status'])
-
-
-@inlineCallbacks
-def _get_statuspage_io_status(target, threshold_time):
-    parsed = yield http_content(target)
-    returnValue(parse_atom_feed(parsed, threshold_time))
-
-
-def get_codecov_status(threshold_time):
-    return _get_statuspage_io_status(
-        target=b'http://status.codecov.io/history.atom',
-        threshold_time=threshold_time
-    )
-
-
-def get_travis_status(threshold_time):
-    return _get_statuspage_io_status(
-        target=b'http://www.traviscistatus.com/history.atom',
-        threshold_time=threshold_time
-    )
+def get_json_status(url):
+    parsed_json = yield http_json(url)
+    returnValue(parsed_json)
 
 
 def red_alert(codecov, travis, github):
-    # return true if an alert should fire.
-    return github == u'bad' or len(travis) > 0 or len(codecov) > 0
+    if (github == GITHUB_GOOD and
+        travis == STATUS_IO_GOOD and
+        codecov == STATUS_IO_GOOD):  # NOQA
+        return GOOD
+    else:
+        return BAD
 
 
 def tweet(message, env=os.environ):
-    twitter = Twython(
-        env['API_KEY'],
-        env['API_SECRET'],
-        env['ACCESS_TOKEN'],
-        env['ACCESS_TOKEN_SECRET']
+    tweeter = Twython(
+        env[b'API_KEY'],
+        env[b'API_SECRET'],
+        env[b'ACCESS_TOKEN'],
+        env[b'ACCESS_TOKEN_SECRET']
     )
-    # try except?
     try:
-        twitter.update_status(status=message)
+        tweeter.update_status(status=message)
     except Exception as e:  # yea, this should be more precise.
-        log.info("{exc}", exc=str(e))
-
-
-def fixed(entries):
-    for entry in entries:
-        entry_html = entry['content'][0]['value']
-        match = REGEX.search(entry_html)
-        if match is not None:
-            return True
-        else:
-            continue
-    return False
+        log.info(u'{exc}', exc=str(e))
 
 
 @inlineCallbacks
-def check_status(context, period):
-    threshold = datetime.now() - timedelta(seconds=period)
+def check_status(context):
 
     travis, codecov, github = yield DeferredList(
         [
-            get_travis_status(threshold),
-            get_codecov_status(threshold),
-            get_github_status()
+            get_json_status(TRAVIS),
+            get_json_status(CODECOV),
+            get_json_status(GITHUB)
         ]
     )
-    new_state = red_alert(codecov[1], travis[1], github[1])
+    parsed_github = parse_github(github[1])
+    parsed_travis = parse_statusio(travis[1])
+    parsed_codecov = parse_statusio(codecov[1])
 
-    codecov_fixed = fixed(codecov[1])
-    travis_fixed = fixed(travis[1])
-    repaired = bool(codecov_fixed or travis_fixed)
+    new_state = red_alert(parsed_codecov, parsed_travis, parsed_github)
 
-    log.info('updated_at={s0_update},s0={s0_alert_state}, s1={s1}',
+    log.info(u'updated_at={s0_update}, s0={s0_state}, s1={s1}',
              s0_update=context.last_update,
-             s0_alert_state=context.alert_state,
+             s0_state=context.state,
              s1=new_state)
 
-    # both are in error state
-    if new_state and context.alert_state:
-        log.info('status={status}', status='still bad')
+    if context.state == BAD and new_state == BAD:
+        log.info('status={status}', status=u'still bad')
 
-    # both are in happy state
-    elif not new_state and not context.alert_state:
-        log.info('status={status}', status='still good')
+    elif context.state == GOOD and new_state == GOOD:
+        log.info(u'status={status}', status=u'still good')
 
-    # happy state to error state
-    elif new_state and not context.alert_state:
-        msg = 'expect problems'
-        log.info('status={status}', status=msg)
+    elif context.state == GOOD and new_state == BAD:
+        msg = u'expect problems'
+        log.info(u'status={status}', status=msg)
         yield deferToThread(tweet, message=msg)
 
-    # error state to happy state
-    elif repaired and context.alert_state:
-        msg = 'Builds should be back to normal'
-        log.info('status={status}', status=msg)
+    elif context.state == BAD and new_state == GOOD:
+        msg = u'Builds should be back to normal'
+        log.info(u'status={status}', status=msg)
         yield deferToThread(tweet, message=msg)
 
-    # update state
-    if repaired:
-        context.alert_state = False
-    else:
-        context.alert_state = new_state
+    context.state = new_state
     context.last_update = datetime.now()
 
     returnValue(context)
 
 
 def run(reactor):
-    context = Context(alert_state=False, last_update=datetime.now())
-    period_seconds = 5 * 60
-    l = LoopingCall(check_status, context, period_seconds)
+    context = Context(state=GOOD, last_update=datetime.now())
+    period_seconds = 2 * 60
+    l = LoopingCall(check_status, context)
     return l.start(period_seconds)
